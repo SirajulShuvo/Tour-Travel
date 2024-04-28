995e06bf6b29a14471a513866b835a6e15a18e94
SirajMia <sirajulshuvo08@gmail.com>
2024-04-28 22:31:47 +0600

    add tour and travel website header and banner

---
+++ index.js
@@ -0,0 +1,42 @@
+// Responsive menubar
+
+let menuClicked = document.getElementById('menuClicked');
+let closeIcon = document.getElementById('closeIcon');
+let mainMenu = document.getElementById('responsive')
+
+
+menuClicked.addEventListener('click',(e) =>{
+   mainMenu.classList.toggle('menuCome');
+})
+
+// select slide and button
+const slideBtn = document.querySelector('.slideBtn');
+const allSlide = Array.from(document.querySelectorAll('.slideA'));
+// console.log(allSlide);
+
+
+// by default slide1 is visible
+allSlide.forEach((slide, index) => {
+    if (index === 0) {
+        slide.style.display = 'block';
+    } else {
+        slide.style.display = 'none';
+    }
+});
+// 
+slideBtn.addEventListener('click',(e) => {
+    if(e.target.dataset.category !== undefined){
+        slideFilter(e.target.dataset.category);
+    }
+})
+
+function slideFilter(value) {
+    allSlide.forEach((currEle) =>{
+        console.log(currEle.dataset.category);
+        if(currEle.dataset.category === value){
+            currEle.style.display = 'block';
+        }else{
+            currEle.style.display = 'none';
+        }
+    })
+}
